eb0436c36f142fca78b11e037fe60c04fcd86ae0
Mark Artishuk <markyshuk@gmail.com>
2020-06-03 15:52:40 -0700

    Removed title from nav bar, changed sizings

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,12 +5,9 @@ const Root = styled.div`
   width: 100%;
   color: white;
   background-color: rgba(0, 0, 0, 0);
-  z-index: 100;
-  transition: background-color 0.35s ease 0s;
-  // background-color: #fbfbfb;
-  height: 55px;
 
   display: flex;
+  height: fit-content;
   align-items: center;
   justify-content: center;
 `;
@@ -24,28 +21,18 @@ const Container = styled.div`
   align-items: baseline;
   // padding: 20px 15px;
   width: 100%;
-  max-width: 2000px;
+  max-width: ${({ theme }) => theme.pageMaxWidth};
 
   font-family: ${(props) => props.theme.textFont};
 
   margin-top: 130px;
 `;
 
-const TitleContainer = styled.div`
-  font-size: 30px;
-  font-weight: bold;
-  text-shadow: 1px 2px #00000066;
-};
-`;
-
-const SimContainer = styled.span``;
-
-const LinksContainer = styled.nav`
+const LinksContainer = styled.div`
   display: flex;
 
   a {
     text-decoration: none;
-    // margin-left: 15px;
     font-size: 16px;
     padding: 7px;
     margin: 7px;
@@ -65,14 +52,12 @@ const NavBar = () => {
   return (
     <Root>
       <Container>
-        {/* <TitleContainer>
-          YPP-<SimContainer>SIM</SimContainer>
-        </TitleContainer> */}
         <LinksContainer>
           <a href="">Play Now</a>
           <a href="">About</a>
           <a href="">Github</a>
         </LinksContainer>
+        {/* <div>Test test 1</div> */}
       </Container>
     </Root>
   );
