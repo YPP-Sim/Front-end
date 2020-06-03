08fd076f3850874c9a79360acf5bb02a7d1a94ef
Mark Artishuk <markyshuk@gmail.com>
2020-06-03 01:45:09 -0700

    Added Button component, added CTA button to hero

---
+++ src/components/Button.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import styled from "styled-components";
+import Values from "values.js";
+
+const Container = styled.button`
+  outline: none;
+  border: none;
+  text-decoration: none;
+
+  background-color: ${(props) =>
+    props.backgroundColor || props.theme.buttonBackgroundColor};
+  color: ${(props) => props.color || props.theme.buttonTextColor};
+
+  // background-image: linear-gradient(0.40turn, #ffa500, #ffcba4);
+
+  font-size: 17.5px;
+
+  // font-family: "Josefin Sans", sans-serif;
+  // font-family: "Montserrat";
+  // font-family: "Quicksand", sans-serif;
+  // font-weight: 300;
+
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  width: ${(props) => props.width || "100%"};
+  height: ${(props) => props.height || "50px"};
+
+  border-radius: ${(props) => props.borderRadius || "3px"};
+  cursor: pointer;
+
+  transition: background-color linear 0.08s;
+  &:hover {
+    background-color: ${(props) => {
+      const val = new Values(
+        props.backgroundColor || props.theme.buttonBackgroundColor
+      ).shade(23);
+      return `#${val.hex}`;
+    }};
+  }
+`;
+
+const Button = (props) => {
+  return <Container {...props}>{props.children}</Container>;
+};
+
+export default Button;
--- src/components/Home/HeroHeader.jsx
+++ src/components/Home/HeroHeader.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import bgImage from "../../images/ship-whee1l.jpg";
+import Button from "../Button";
 
 const Root = styled.div`
   background: radial-gradient(
       at center center,
-      rgba(0, 0, 0, 0) 0%,
-      rgba(0, 0, 0, 0.1) 65%,
+      rgba(0, 0, 0, 0.1) 0%,
+      rgba(0, 0, 0, 0.2) 65%,
       rgba(0, 0, 0, 0.5) 100%
     ),
     url(${bgImage});
@@ -18,8 +19,34 @@ const Root = styled.div`
   height: 100vh;
 `;
 
+const CenterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  // transform: translateY(-80px);
+`;
+
+const HeroButton = styled(Button)`
+  width: 90%;
+  height: 56px;
+  max-width: 450px;
+  font-size: 21px;
+  // text-transform: uppercase;
+`;
+
 const HeroHeader = () => {
-  return <Root></Root>;
+  return (
+    <Root>
+      <CenterContainer>
+        <HeroButton backgroundColor="#4663ac" color="#fff">
+          Play Now
+        </HeroButton>
+      </CenterContainer>
+    </Root>
+  );
 };
 
 export default HeroHeader;
