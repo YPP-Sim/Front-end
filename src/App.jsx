3358048573c4a965674bd0f7574336a20beeda59
Mark Artishuk <markyshuk@gmail.com>
2020-06-03 22:56:30 -0700

    Added a new navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import HomePage from "./components/Home/HomePage";
 import { Route } from "react-router-dom";
 import GameList from "./components/GameList/GameList";
+import NavBar from "./components/NavBar";
 const Root = styled.div``;
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       </Route>
 
       <Route exact path="/games">
+        <NavBar />
         <GameList />
       </Route>
     </Root>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Root = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: #fafafa;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LinksContainer = styled.nav`
+  a {
+    color: ${({ theme }) => theme.textColor};
+    text-decoration: none;
+    font-family: ${({ theme }) => theme.textFont};
+
+    padding: 7px;
+    margin: 7px;
+  }
+`;
+
+const NavBar = () => {
+  return (
+    <Root>
+      <LinksContainer>
+        <Link to="/">Home</Link>
+        <Link to="/games">Game List</Link>
+        <a href="https://github.com/YPP-Sim">Github</a>
+      </LinksContainer>
+    </Root>
+  );
+};
+
+export default NavBar;
