07bddda35aa05241924b9d18a4658ee60baf9912
Mark Artishuk <markyshuk@gmail.com>
2020-06-27 19:12:43 -0700

    Refactored SpriteBody to store children in an object instead of an array

--- src/game/SpriteBody.js
+++ src/game/SpriteBody.js
@@ -3,6 +3,7 @@
  */
 
 import { Sprite } from "pixi.js";
+import uniqid from "uniqid";
 /**
  * @class A helpful class that groups sprites so that they can move together.
  */
@@ -21,34 +22,32 @@ class SpriteBody {
       this.mainSprite.x = x;
       this.mainSprite.y = y;
     }
-    this.children = [];
-    this.spriteCounter = 0;
+    this.children = {};
   }
 
   addSprite(sprite, xOffset, yOffset) {
     sprite.x = this.x + xOffset;
     sprite.y = this.y + yOffset;
+    const spriteIdentifier = uniqid("spritebody-");
+    sprite.bodyId = spriteIdentifier;
 
-    this.children.push({
+    this.children[spriteIdentifier] = {
       sprite,
       xOffset,
       yOffset,
-    });
-    this.spriteCounter++;
-
-    const index = this.spriteCounter - 1;
+    };
 
     const setSpriteOffset = (offsetX, offsetY) => {
       sprite.x = this.x + offsetX;
       sprite.y = this.y + offsetY;
 
-      const spriteObj = this.children[index];
+      const spriteObj = this.children[spriteIdentifier];
       spriteObj.xOffset = offsetX;
       spriteObj.yOffset = offsetY;
     };
 
     const incrementSpriteOffset = (incrementX, incrementY) => {
-      const spriteObj = this.children[index];
+      const spriteObj = this.children[spriteIdentifier];
       if (!spriteObj) return;
       spriteObj.xOffset += incrementX;
       spriteObj.yOffset += incrementY;
@@ -58,8 +57,7 @@ class SpriteBody {
     };
 
     const removeSprite = () => {
-      const spriteObj = this.children[index];
-      this.children.splice(index, 1);
+      delete this.children[spriteIdentifier];
     };
 
     return { setSpriteOffset, incrementSpriteOffset, removeSprite };
@@ -77,7 +75,7 @@ class SpriteBody {
 
     let spriteChild;
     //Change position of children to stick with body
-    for (spriteChild of this.children) {
+    for (spriteChild of Object.values(this.children)) {
       spriteChild.sprite.x = this.x + spriteChild.xOffset;
       spriteChild.sprite.y = this.y + spriteChild.yOffset;
     }
