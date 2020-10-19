6c0ada79e15cd232d4bf1ba22637950517045772
Mark Artishuk <markyshuk@gmail.com>
2020-10-19 01:00:26 -0700

    Further improvements to the protected route component

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import UserContext from "../contexts/UserContext";
 import {Redirect, Route} from "react-router-dom";
 import {getAccessToken} from "../util/TokenStorage";
 
 const ProtectedRoute = ({children, ...rest}) => {
+    const {loggedIn} = useContext(UserContext);
     return ( <Route {...rest} >
-        {getAccessToken() ? children : (
+        {getAccessToken() || loggedIn ? children : (
             <Redirect to="/login" />
         )}
     </Route> );
