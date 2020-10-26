3cc186f72900654afcc4a5bfb96f7fd98b330f2b
Mark Artishuk <markyshuk@gmail.com>
2020-10-26 15:09:48 -0700

    Added spacing to register/login forms

--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -3,18 +3,19 @@ import styled from "styled-components";
 import LoginForm from "./LoginForm";
 
 const Root = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 125px;
 `;
 
-
 const LoginPage = () => {
-  return <Root>
-        <LoginForm /> 
-  </Root>;
+  return (
+    <Root>
+      <LoginForm />
+    </Root>
+  );
 };
 
 export default LoginPage;
--- src/components/Register/RegisterPage.jsx
+++ src/components/Register/RegisterPage.jsx
@@ -3,18 +3,19 @@ import styled from "styled-components";
 import RegisterForm from "./RegisterForm";
 
 const Root = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 125px;
 `;
 
-
 const RegisterPage = () => {
-  return <Root>
+  return (
+    <Root>
       <RegisterForm />
-  </Root>;
+    </Root>
+  );
 };
 
 export default RegisterPage;
